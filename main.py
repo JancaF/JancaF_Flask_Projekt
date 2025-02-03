7869fdc16f9be9ce3c90b2bc701070f1f510a348
JancaF <filip.janca@student.ossp.cz>
2025-02-03 14:04:19 +0100

    pridani view a šablona templates

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def index():
 def kontakty():
     return '<h1>Kontakty</h1> <a href="/">Domů</a>'
 
+@app.route('/nasobek/<cislo1>/<cislo2>)')
+def cislo(cislo1, cislo2):
+    try:
+        return f'Násobek je {int(cislo1) * int(cislo2)}'
+    except ValueError:
+        return 'Neplatné číslo.'
+@app.route('/<jmeno>/<prijmeni>')
+def uzivatel(jmeno, prijmeni):
+    return f'<h2> Vítej {jmeno} {prijmeni}</h2>'
 @app.route('/<jmeno>')
 def jmeno(jmeno):
     return f'<h2> Vítej {jmeno} </h2>'
