1a994b6fd8f6cc7db1b356ffde9687c86435b7de
JancaF <filip.janca@student.ossp.cz>
2025-02-10 14:28:44 +0100

    Napracování ostatních stránek abecedy, a upravení base.html kvůli problému tagů {% block %} (hotfix)

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('base.html')
+@app.route('/homepage')
+def homepage():
     return render_template('index.html')
-
+@app.route('/abeceda/')
+def abeceda():
+    return render_template('abeceda.html')
+@app.route('/alfabeta/')
+def alfabeta():
+    return render_template('alfabeta.html')
+@app.route('/azbuka/')
+def azbuka():
+    return render_template('azb.html')
+@app.route('/hebrejstina/')
+def hebrejstina():
+    return render_template('hebrej.html')
 @app.route('/kontakty')
 def kontakty():
     return '<h1>Kontakty</h1> <a href="/">Domů</a>'
-
 @app.route('/gallery/')
 def gallery():
-    return '<h1>Galerie</h1>'
+    return '<h1>Galerie</h1> <a href="/">Domů</a>'
+@app.route('/nasobek/')
+def nasobek():
+    return '<h1>Nasobek</h1> <a href="/">Domů</a>'
 @app.route('/nasobek/<a>/<b>)')
 def number(a, b):
     try:
