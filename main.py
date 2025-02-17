056ce49d332ac60f96d3a9191696c0d0705e144c
filip.janca <filip.janca@student.ossp.cz>
2025-02-17 10:10:08 +0100

    Práce s formulářema

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('base.html')
+@app.route('/homepage')
+def homepage():
     return render_template('index.html')
-@app.route('/abeceda/abc')
+@app.route('/abeceda/')
 def abeceda():
     return render_template('abeceda.html')
-@app.route('/alfabeta/alfa')
+@app.route('/alfabeta/')
 def alfabeta():
     return render_template('alfabeta.html')
-@app.route('/azbuka/azb')
-def azbuka():
-    return render_template('azb.html')
-@app.route('hebrejstina/heb')
+@app.route('/hebrejstina/')
 def hebrejstina():
     return render_template('hebrej.html')
 @app.route('/kontakty')
 def kontakty():
     return '<h1>Kontakty</h1> <a href="/">Domů</a>'
-
-@app.route('/nasobek/<cislo1>/<cislo2>)')
-def cislo(cislo1, cislo2):
+@app.route('/gallery/')
+def gallery():
+    return '<h1>Galerie</h1> <a href="/">Domů</a>'
+@app.route('/nasobek/')
+def nasobek():
+    return '<h1>Nasobek</h1> <a href="/">Domů</a>'
+@app.route('/nasobek/<a>/<b>)')
+def number(a, b):
     try:
-        return f'Násobek je {int(cislo1) * int(cislo2)}'
+        return f'Násobek je {int(a) * int(b)}'
     except ValueError:
         return 'Neplatné číslo.'
-@app.route('user/<jmeno>/<prijmeni>')
-def uzivatel(jmeno, prijmeni):
-    return f'<h2> Vítej {jmeno} {prijmeni}</h2>'
+@app.route('/user/<name>/<surname>')
+def uzivatel(name, surname):
+    return f'<h2> Vítej {name} {surname}</h2>'
+@app.route('/odkaz', methods=['GET', 'POST'])
+def link():
+    if request.method == 'POST':
+        blog = request.form['blog']
+        email = request.form['email']
+        radio = request.form['radio']
+        return render_template('zkouska.html', blog=blog, email=email, radio=radio)
+    return render_template('link.html')
+@app.route('/<name>')
+def username(name):
+    return f'<h2> Vítej {name} </h2>'
 
-@app.route('/<jmeno>')
-def jmeno(jmeno):
-    return f'<h2> Vítej {jmeno} </h2>'
 if __name__ == '__main__':
     app.run(debug=True)
