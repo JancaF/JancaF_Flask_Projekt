0aca7ed7b40e16fbccef334bfade8dcebce45a79
filip.janca <filip.janca@student.ossp.cz>
2025-02-17 10:10:08 +0100

    Práce s formulářema

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('base.html')
+@app.route('/homepage')
+def homepage():
     return render_template('index.html')
-
+@app.route('/abeceda/')
+def abeceda():
+    return render_template('abeceda.html')
+@app.route('/alfabeta/')
+def alfabeta():
+    return render_template('alfabeta.html')
+@app.route('/hebrejstina/')
+def hebrejstina():
+    return render_template('hebrej.html')
 @app.route('/kontakty')
 def kontakty():
     return '<h1>Kontakty</h1> <a href="/">Domů</a>'
@@ -23,9 +34,17 @@ def number(a, b):
 @app.route('/user/<name>/<surname>')
 def uzivatel(name, surname):
     return f'<h2> Vítej {name} {surname}</h2>'
-
+@app.route('/odkaz', methods=['GET', 'POST'])
+def link():
+    if request.method == 'POST':
+        blog = request.form['blog']
+        email = request.form['email']
+        radio = request.form['radio']
+        return render_template('zkouska.html', blog=blog, email=email, radio=radio)
+    return render_template('link.html')
 @app.route('/<name>')
 def username(name):
     return f'<h2> Vítej {name} </h2>'
+
 if __name__ == '__main__':
     app.run(debug=True)
