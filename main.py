74cbfa806ff60536ea3c6be585a0926879ba253b
JancaF <filip.janca@student.ossp.cz>
2025-02-17 14:23:48 +0100

    Zpracovani ukolu meho tematu s funkcnim prihlasenim

--- main.py
+++ main.py
@@ -7,40 +7,13 @@ def index():
 @app.route('/homepage')
 def homepage():
     return render_template('index.html')
-@app.route('/abeceda/')
-def abeceda():
-    return render_template('abeceda.html')
-@app.route('/alfabeta/')
-def alfabeta():
-    return render_template('alfabeta.html')
-@app.route('/hebrejstina/')
-def hebrejstina():
-    return render_template('hebrej.html')
-@app.route('/kontakty')
-def kontakty():
-    return '<h1>Kontakty</h1> <a href="/">Domů</a>'
-@app.route('/gallery/')
-def gallery():
-    return '<h1>Galerie</h1> <a href="/">Domů</a>'
-@app.route('/nasobek/')
-def nasobek():
-    return '<h1>Nasobek</h1> <a href="/">Domů</a>'
-@app.route('/nasobek/<a>/<b>)')
-def number(a, b):
-    try:
-        return f'Násobek je {int(a) * int(b)}'
-    except ValueError:
-        return 'Neplatné číslo.'
-@app.route('/user/<name>/<surname>')
-def uzivatel(name, surname):
-    return f'<h2> Vítej {name} {surname}</h2>'
+@app.route('/obchod')
+def obchod():
+    return render_template('shop.html')
 @app.route('/odkaz', methods=['GET', 'POST'])
-def link():
+def link(user='pokuston', password='heslo'):
     if request.method == 'POST':
-        user = request.form['user']
-        password = request.form['password']
-        radio = request.form['radio']
-        return render_template('zkouska.html', user=user, password=password, radio=radio)
+        return render_template('login.html', user=user, password=password)
     return render_template('link.html')
 @app.route('/<name>')
 def username(name):
