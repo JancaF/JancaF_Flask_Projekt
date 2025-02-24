49bdf019c238d709c0514d5726186dc7018cf1e4
filip.janca <filip.janca@student.ossp.cz>
2025-02-24 10:24:21 +0100

    Ukol 2 - Vypracování flashmessage se stylováním a vstupem z formuláře

--- main.py
+++ main.py
@@ -1,50 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "dev"
 @app.route('/')
 def index():
     return render_template('base.html')
 @app.route('/homepage')
 def homepage():
     return render_template('index.html')
-@app.route('/abeceda/')
-def abeceda():
-    return render_template('abeceda.html')
-@app.route('/alfabeta/')
-def alfabeta():
-    return render_template('alfabeta.html')
-@app.route('/hebrejstina/')
-def hebrejstina():
-    return render_template('hebrej.html')
-@app.route('/kontakty')
-def kontakty():
-    return '<h1>Kontakty</h1> <a href="/">Domů</a>'
-@app.route('/gallery/')
-def gallery():
-    return '<h1>Galerie</h1> <a href="/">Domů</a>'
-@app.route('/nasobek/')
-def nasobek():
-    return '<h1>Nasobek</h1> <a href="/">Domů</a>'
-@app.route('/nasobek/<a>/<b>)')
-def number(a, b):
-    try:
-        return f'Násobek je {int(a) * int(b)}'
-    except ValueError:
-        return 'Neplatné číslo.'
-@app.route('/user/<name>/<surname>')
-def uzivatel(name, surname):
-    return f'<h2> Vítej {name} {surname}</h2>'
 @app.route('/odkaz', methods=['GET', 'POST'])
 def link():
     if request.method == 'POST':
-        user = request.form['user']
-        password = request.form['password']
-        radio = request.form['radio']
-        return render_template('zkouska.html', user=user, password=password, radio=radio)
-    return render_template('link.html')
-@app.route('/<name>')
-def username(name):
-    return f'<h2> Vítej {name} </h2>'
+        if request.form["username"] == "pokuston" and request.form["password"] == "heslo":
+            flash("Login successful", "message")
+            return redirect(url_for('index'))
+        flash("Login failed", "warning")
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
