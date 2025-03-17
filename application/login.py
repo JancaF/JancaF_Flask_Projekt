ee6157780b3188cee93339487d7cbe0e7b9dff66
JancaF <filip.janca@student.ossp.cz>
2025-03-17 14:32:15 +0100

    KONZULTACE PRÁCE + ROZPRACOVÁNÍ LOGIN-LOGOUT

---
+++ application/login.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, redirect, render_template, request, url_for, session, flash
+
+from application import db_execute
+
+bp = Blueprint('login', __name__, url_prefix='/login')
+
+USERS = {"pokuston": "kouzelnik", "admin": "admin", "student":"zak"}
+
+@bp.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        command = "SELECT username FROM users WHERE username = ? and password = ?"
+        result = db_execute(command, (username, password))
+
+        if result:
+            session["user"] = username
+            flash("Login successful", "message")
+            return redirect(url_for('index'))
+
+        flash("Login failed", "warning")
+
+    return render_template('login.html')
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        register_command = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)"
+        result = db_execute(register_command, (username, password, email))
+
+
+        if request.form == ["username", "password", "email"]:
+            if USERS[request.form['username']] == username and USERS[request.form['password']] == password and USERS[request.form['email']] == email:
+                flash("Registration successful", "message")
+                return redirect(url_for('index'))
+            else:
+                flash("Registration failed", "warning")
+    return render_template('register.html')
+
+@bp.route("/users")
+def user_list():
+    command = "SELECT username, password FROM users"
+    results = db_execute(command)
+    print(results)
+    return render_template("user.html", results=results)
+
+@bp.route("/homepage")
+def homepage():
+    return render_template("index.html")
+
+@bp.route('/logout')
+def logout():
+    session.pop("user", None)  # Odstraní uživatele ze session
+    flash("Odhlášení bylo úspěšné", "message")
+    return redirect(url_for('login.login'))
+
+@bp.route("/shop")
+def shop():
+    product_command = "SELECT name, price FROM products"
+    results = db_execute(product_command)
+    print(results)
+    return render_template("shop.html", results=results)
